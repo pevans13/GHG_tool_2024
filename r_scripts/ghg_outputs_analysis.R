a809868d1bef63496490a888bb538b596c27f810
Paul Evans <paueva@ceh.ac.uk>
2024-07-23 09:12:52 +0100

    analysis script

---
+++ r_scripts/ghg_outputs_analysis.R
@@ -0,0 +1,49 @@
+## ---------------------------
+##
+## Script name: ghg_outputs_analysis.R
+##
+## Purpose of script: to create the final figures to use in the paper of the model
+##                    
+## ------------ Notes --------------  ##
+## The figures will include the 2015 data, and also the 
+## scenarios that were used. See Redhead et al. (2020). 
+
+## The final maps were already produced in the 'total_ghg_script.R' script.
+## ------------ ----- --------------  ##
+##
+## Run after: total_ghg_script.R
+##
+## Specific figures:
+## 1 - 
+## 2 - 
+## 3 - 
+## 4 - 
+##
+## Author: Dr. Paul M. Evans
+##
+## Date Created: 2024-07-23
+##
+## Copyright (c) Paul M. Evans, 2023
+## Email: paueva@ceh.ac.uk
+## Git: https://github.com/pevans13
+
+## ---------------------------
+options(scipen = 6, digits = 4) # for non-scientific notation
+## ---------------------------
+
+# remove objects
+rm(list = ls())
+
+#### 0 - paths/ names ####
+spatialGrid <- "agland_grid_ESW.gpkg"
+
+#### 0 - load libraries ####
+source("./r_scripts/proj_packages.R")
+
+#### 0 - load functions ####
+
+#### 1 - task1 ####
+
+#### 2 - task2 ####
+
+#### 3 - task3 ####
